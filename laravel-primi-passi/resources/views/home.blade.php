150cc7bff3a21a66b489f6d1919053636fcde98f
Samuele-Chioda <samuelechioda@gmail.com>
2024-07-12 16:46:23 +0200

    aggiunta dati in modo dinamico

--- laravel-primi-passi/resources/views/home.blade.php
+++ laravel-primi-passi/resources/views/home.blade.php
@@ -1,28 +1,14 @@
-
-Esercizio di oggi: Laravel Primi Passi
-nome repo: laravel-primi-passi
-Per prima cosa, creiamo un nuovo progetto Laravel 10, utilizzando questo comando:
-composer create-project laravel/laravel:^10.0 laravel-primi-passi
-Al termine dell'installazione, entriamo nella cartella del progetto cd laravel-primi-passi e avviamo l'artisan serve con uno di questi due comandi:
-php artisan serve oppure php -S localhost:8000 -t public
-A questo punto, iniziamo a prendere confidenza con le rotte e le views:
-cancelliamo la view welcome.blade.php e creiamo una nostra homepage.
-Facciamo quindi sì che la rotta / visualizzi home.blade.php
-Inizialmente stampiamo un Hello World, poi passiamo dei dati alla view in modo da visualizzarli dinamicamente con Blade.
-Bonus:
-Creiamo più di una pagina e visualizziamo un header menu con i link di tutte le pagine, utilizzando la funzione route()
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>laraverl-primi-passi</title>
+    <title>laravel-primi-passi</title>
 </head>
 <body>
     <h1>
-        HELLO WORLD!
+        {{ $messaggio }}
     </h1>
 </body>
 </html>
